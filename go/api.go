fd5ddb1df01e6f6a35c00cf12281e3e464f6742d
Dag Sverre Seljebotn <dag.sverre.seljebotn@vipps.no>
2023-02-22 09:44:17 +0100

    Refactor Handler to provide lower-level HandlerWithoutRoute

--- go/api.go
+++ go/api.go
@@ -160,62 +160,77 @@ func (page *EventPageSingleType[T]) Event(partitionID int, h map[string]string, 
 	return nil
 }
 
-// Handler wraps API in a http.Handler.
+// HandlerWithoutRoute wraps API in a http.Handler that implements the
+// ZeroEventHub HTTP protocol. The path/method is not checked. Use this method
+// to plug a handler into your own service routing.
+func HandlerWithoutRoute(api API, getLogger func(request *http.Request) logrus.FieldLogger) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		logger := getLogger(request)
+		query := request.URL.Query()
+		if !query.Has("n") {
+			http.Error(writer, ErrHandshakePartitionCountMissing.Error(), ErrHandshakePartitionCountMissing.Status())
+			return
+		}
+		if n, err := strconv.Atoi(query.Get("n")); err != nil {
+			http.Error(writer, err.Error(), http.StatusBadRequest)
+			return
+		} else {
+			if n != api.GetPartitionCount() {
+				http.Error(writer, ErrHandshakePartitionCountMismatch.Error(), ErrHandshakePartitionCountMismatch.Status())
+				return
+			}
+		}
+		var pageSizeHint int
+		if query.Has("pagesizehint") {
+			if x, err := strconv.Atoi(query.Get("pagesizehint")); err != nil {
+				http.Error(writer, err.Error(), http.StatusBadRequest)
+				return
+			} else {
+				pageSizeHint = x
+			}
+		}
+		var headers []string
+		if query.Has("headers") {
+			headers = strings.Split(strings.TrimSuffix(query.Get("headers"), ","), ",")
+		}
+		cursors, err := parseCursors(api.GetPartitionCount(), query)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusBadRequest)
+			return
+		}
+		fields := logger.
+			WithField("event", api.GetName()).
+			WithField("PartitionCount", api.GetPartitionCount()).
+			WithField("Cursors", cursors).
+			WithField("PageSizeHint", pageSizeHint).
+			WithField("Headers", headers)
+		fields.Info()
+		serializer := NewNDJSONEventSerializer(writer)
+		err = api.FetchEvents(request.Context(), cursors, pageSizeHint, serializer, headers...)
+		if err != nil {
+			logger.WithField("event", api.GetName()+".fetch_events_error").WithError(err).Info()
+			http.Error(writer, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+	})
+}
+
+// Handler wraps API in a http.Handler that checks the path and method (GET)
+// in addition to serving the feed like HandlerWithoutRoute does.
+// Note: `logger` is also hardcoded if you use this function; use HandlerWithoutRoute
+// directly to also be able to configure the logger per request to e.g.
+// include request IDs in log output.
 func Handler(path string, logger logrus.FieldLogger, api API) http.Handler {
 	if logger == nil {
 		logger = logrus.StandardLogger()
 	}
+	getLogger := func(*http.Request) logrus.FieldLogger {
+		return logger
+	}
 	router := mux.NewRouter()
 	router.Methods(http.MethodGet).
 		Path(path).
-		HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-			query := request.URL.Query()
-			if !query.Has("n") {
-				http.Error(writer, ErrHandshakePartitionCountMissing.Error(), ErrHandshakePartitionCountMissing.Status())
-				return
-			}
-			if n, err := strconv.Atoi(query.Get("n")); err != nil {
-				http.Error(writer, err.Error(), http.StatusBadRequest)
-				return
-			} else {
-				if n != api.GetPartitionCount() {
-					http.Error(writer, ErrHandshakePartitionCountMismatch.Error(), ErrHandshakePartitionCountMismatch.Status())
-					return
-				}
-			}
-			var pageSizeHint int
-			if query.Has("pagesizehint") {
-				if x, err := strconv.Atoi(query.Get("pagesizehint")); err != nil {
-					http.Error(writer, err.Error(), http.StatusBadRequest)
-					return
-				} else {
-					pageSizeHint = x
-				}
-			}
-			var headers []string
-			if query.Has("headers") {
-				headers = strings.Split(strings.TrimSuffix(query.Get("headers"), ","), ",")
-			}
-			cursors, err := parseCursors(api.GetPartitionCount(), query)
-			if err != nil {
-				http.Error(writer, err.Error(), http.StatusBadRequest)
-				return
-			}
-			fields := logger.
-				WithField("event", api.GetName()).
-				WithField("PartitionCount", api.GetPartitionCount()).
-				WithField("Cursors", cursors).
-				WithField("PageSizeHint", pageSizeHint).
-				WithField("Headers", headers)
-			fields.Info()
-			serializer := NewNDJSONEventSerializer(writer)
-			err = api.FetchEvents(request.Context(), cursors, pageSizeHint, serializer, headers...)
-			if err != nil {
-				logger.WithField("event", api.GetName()+".fetch_events_error").WithError(err).Info()
-				http.Error(writer, "Internal server error", http.StatusInternalServerError)
-				return
-			}
-		})
+		Handler(HandlerWithoutRoute(api, getLogger))
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		router.ServeHTTP(writer, request)
 	})
